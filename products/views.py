bcc1b1fe20438d10d025af2d7d3d94f50d01fda1
Luigi Langella <luigi76langella@gmail.com>
2020-06-23 10:56:25 +0100

    Added product filtering functionality

--- products/views.py
+++ products/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 def all_products(request):
     """ A view that will display all the products and allow search queries"""
 
     products = Product.objects.all()
     query = None
+    categories = None
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -21,6 +26,7 @@ def all_products(request):
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'products/products.html', context)
